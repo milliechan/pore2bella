24a9bf2f002c2c1e8dc38b87443c44747b455470
eyingling02 <eyingling02@gmail.com>
2017-04-08 18:13:21 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res, next) {
-  try{
-    const html = (__dirname + '/index.html')
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(process.env.PORT || 3000, function() {
